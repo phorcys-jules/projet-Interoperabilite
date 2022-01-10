f0f11252bac5d1519b97292bc68ab02bc5bdb917
Jules <jfrancois968@gmail.com>
2022-01-10 15:53:07 +0100

    ✨ link map to localisation

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,19 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicGhvcmN5cy1qdWxlcyIsImEiOiJja3k4bnhsN2wxZmtqMnZveWRoMTNieWFnIn0.NJPsNkYoDfVjAh11iCqEUQ';
-let map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9 // starting zoom
-});
+function afficherMap(lat, long) {
+    console.log("display the map...")
+    
+    mapboxgl.accessToken = 'pk.eyJ1IjoicGhvcmN5cy1qdWxlcyIsImEiOiJja3k4bnhsN2wxZmtqMnZveWRoMTNieWFnIn0.NJPsNkYoDfVjAh11iCqEUQ';
+    let map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [lat, long], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+    });
 
-map.transform.center.lat = 0;
-map.transform.center.lng = 0;
+    map.transform.center.lat = 0;
+    map.transform.center.lng = 0;
 
 
-console.log(map.transform.center);
-/*
-var map = L.map('map').setView([51.505, -0.09], 13);
+    console.log(map.transform.center);
+}
 
-//TODO En récupérant par API, en JSON, les coordonnées de la mairie de Notre Dame des Landes
-L.tileLayer('https://api.mapbox.com/styles/v1/phorcys-jules/cky8nz6fv738s15nuwlk0h79n.html?title=view&access_token=pk.eyJ1IjoicGhvcmN5cy1qdWxlcyIsImEiOiJja3k4bnU5cjUwMWk5MnZsYmp1am5pem04In0.LTlGjtYT-0MdURTgGk9fDA&zoomwheel=true&fresh=true#12/48.8665/2.3176', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoicGhvcmN5cy1qdWxlcyIsImEiOiJja3k4bnhsN2wxZmtqMnZveWRoMTNieWFnIn0.NJPsNkYoDfVjAh11iCqEUQ'
-}).addTo(map);
-
-*/
+console.log("app.js");
--- src/php/Circulation.php
+++ src/php/Circulation.php
@@ -1,15 +1,55 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Circulation</title>
+    <link href="https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css" rel="stylesheet">
+    <script src="https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.js"></script>
+    <script type="text/javascript" >
+        function afficherMap(lat, long) {
+            console.log("display the map...");
+            
+            mapboxgl.accessToken = 'pk.eyJ1IjoicGhvcmN5cy1qdWxlcyIsImEiOiJja3k4bnhsN2wxZmtqMnZveWRoMTNieWFnIn0.NJPsNkYoDfVjAh11iCqEUQ';
+            let map = new mapboxgl.Map({
+                container: 'map', // container ID
+                style: 'mapbox://styles/mapbox/streets-v11', // style URL
+                center: [long, lat], // starting position [lng, lat]
+                zoom: 17 // starting zoom
+            });
+
+            console.log(map.transform.center);
+        }
+
+    </script>
+</head>
+<link rel="stylesheet" href="../css/style.css">
+<body>
+    <div id="map">
 
 
-$address ="Notre dame des landes";
-$prepAddr = str_replace(' ','+',$address);
-$geocode=file_get_contents('https://api-adresse.data.gouv.fr/search/?q=Mairie+Notre+Dames+des+Landes'.$prepAddr) or die("Impossible d'acceder aux services de géolocalisation");
-$output= json_decode($geocode);
+    </div>
+    
+    <?php
+        $address ="Mairie Notre dame des landes";
+        $prepAddr = str_replace(' ','+',$address);
+        $geocode=file_get_contents('https://api-adresse.data.gouv.fr/search/?q=Mairie+Notre+Dames+des+Landes'.$prepAddr) or die("Impossible d'acceder aux services de géolocalisation");
+        $output= json_decode($geocode);
 
-$latitude = $output->features[0]->geometry->coordinates[0];
-$longitude = $output->features[0]->geometry->coordinates[1];
+        $latitude = $output->features[0]->geometry->coordinates[1];
+        $longitude = $output->features[0]->geometry->coordinates[0];
+
+        echo 'sah';
+        echo "<script type='text/javascript'>afficherMap(${latitude}, ${longitude});</script>";
+        //echo '<script type="text/javascript">afficherMap(1,2);</script>';
+
+    ?>
+
+</body>
 
 
-echo $latitude.'<br>';
-echo $longitude;
-?>
+
+
+
+</html>
