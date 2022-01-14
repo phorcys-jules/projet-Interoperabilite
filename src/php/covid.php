f33371e45536be0c08b5ee9d05331ff826a6e478
Jules <jfrancois968@gmail.com>
2022-01-14 10:55:22 +0100

    ✨ get covid data

--- src/php/covid.php
+++ src/php/covid.php
@@ -10,13 +10,14 @@ stream_context_set_default($opts);
 $ipURI = "http://ip-api.com/xml/?lang=fr";
 $geolocData = simplexml_load_string(file_get_contents($ipURI));
 $coo = $geolocData->lat . "," . $geolocData->lon;
+$dep = $geolocData->zip;
+$dep = substr($dep,0, 2);
+echo 'Vous avez été localisé dans le departement '.$dep.'<br>';
 
-echo $coo;
 
 
 
-
-$filename = 'https://www.data.gouv.fr/fr/datasets/r/a1466f7f-4ece-4158-a373-f5d4db167eb0';
+$filename = 'https://www.data.gouv.fr/fr/datasets/r/5c4e1452-3850-4b59-b11c-3dd51d7fb8b5';
 $data = [];
 
 // open the file
@@ -28,13 +29,16 @@ if ($f === false) {
 
 // read each line in CSV file at a time
 while (($row = fgetcsv($f)) !== false) {
-	echo $row->jour;
+	if ($row[0] == '54') { 
+		//date, taux incidence, nb positif
+		array_push($data,[$row[1], $row[7], $row[20]]);
+	}
 }
+
+var_dump($data);
 
 // close the file
 fclose($f);
-
-echo 'csv';
 
 ?>
 
