64cf2b93961be2074d1267f37656f83f6dcc9cde
Wilt Lilian <lilian.wilt@hotmail.com>
2022-01-14 19:21:33 +0100

    Reorganisation circulations.php

--- Interop/circulations.php
+++ Interop/circulations.php
@@ -9,67 +9,10 @@
   <link href="https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css" rel="stylesheet">
   <script src="https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-  <script type="text/javascript">
-    let map;
-    //Affiche la carte centré sur la position en paramètre
-    function afficherMap(lat, long) {
-      console.log("display the map...");
+  <script src="./js/circulations.js"></script>
 
-      mapboxgl.accessToken = 'pk.eyJ1IjoicGhvcmN5cy1qdWxlcyIsImEiOiJja3k4bnhsN2wxZmtqMnZveWRoMTNieWFnIn0.NJPsNkYoDfVjAh11iCqEUQ';
-      map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: [long, lat], // starting position [lng, lat]
-        zoom: 17 // starting zoom
-      });
-    }
-
-    function createPoint(lat, long, p_titre, p_descritpion) {
-      //console.log("place point...", lat, long, p_titre, p_descritpion);
-
-      const geojson = {
-        type: 'FeatureCollection',
-        features: [{
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [long, lat]
-          },
-          properties: {
-            title: p_titre,
-            description: p_descritpion
-          }
-        }]
-      };
-
-      // add markers to map
-      for (const feature of geojson.features) {
-        // create a HTML element for each feature
-        const el = document.createElement('div');
-        el.className = 'marker';
-
-        // make a marker for each feature and add to the map
-        new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
-      }
-    }
-    let labels = [];
-    let datas = [];
-    let datas2 = [];
-    
-    function configChart(param) {
-      param.forEach(function(item){
-        //date
-        labels.push(item[0]);
-        //taux inci
-        datas.push(item[1]);
-        //nb cas >0
-        datas2.push(item[2]);
-
-      });
-    }
-  </script>
 </head>
-<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="./css/style.css">
 
 <body>
   <div id="map">
---
+++ Interop/js/circulations.js
@@ -0,0 +1,58 @@
+let map;
+//Affiche la carte centré sur la position en paramètre
+function afficherMap(lat, long) {
+  console.log("display the map...");
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoicGhvcmN5cy1qdWxlcyIsImEiOiJja3k4bnhsN2wxZmtqMnZveWRoMTNieWFnIn0.NJPsNkYoDfVjAh11iCqEUQ';
+  map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [long, lat], // starting position [lng, lat]
+    zoom: 17 // starting zoom
+  });
+}
+
+function createPoint(lat, long, p_titre, p_descritpion) {
+  //console.log("place point...", lat, long, p_titre, p_descritpion);
+
+  const geojson = {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [long, lat]
+      },
+      properties: {
+        title: p_titre,
+        description: p_descritpion
+      }
+    }]
+  };
+
+  // add markers to map
+  for (const feature of geojson.features) {
+    // create a HTML element for each feature
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
+  }
+}
+let labels = [];
+let datas = [];
+let datas2 = [];
+
+function configChart(param) {
+  param.forEach(function(item){
+    //date
+    labels.push(item[0]);
+    //taux inci
+    datas.push(item[1]);
+    //nb cas >0
+    datas2.push(item[2]);
+
+  });
+}
+
